2f3200a2f56f59f3a5bac2857ad8b3cc56f75f56
Pablo <bluesd@users.noreply.github.com>
2017-06-13 14:16:59 -0300

    "EMACS Agregados y configurados plugins"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,14 +13,17 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
   '(package-selected-packages
-    (quote (darktooth-theme
-	    magit git-gutter
-	    neotree fill-column-indicator
-	    evil evil-commentary evil-surround move-text
+    (quote (darktooth-theme spaceline diminish
+            rainbow-delimiters
+            neotree fill-column-indicator
+            evil evil-leader evil-commentary evil-surround
+            avy move-text
+            magit git-gutter
          ;; === Modes
             web-mode
 	    php-mode
 	    js2-mode
+            groovy-mode
 	    ))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
@@ -40,6 +43,7 @@
 
 ;; ===========// STYLE //============
 (tool-bar-mode -1)
+(toggle-scroll-bar -1)
 (load-theme 'darktooth t)
 (add-to-list 'default-frame-alist
                        '(font . "Source Code Pro-10"))
@@ -51,17 +55,52 @@
 ;; (global-linum-mode t)
 
 ;; ========// CFG PLUGINS //=========
-;; ====== NeoTree
-(setq neo-theme 'arrow)
-;; ====== GitGutter
+;; ------ FillColumn
+(setq fci-rule-image-format 'pbm)
+(setq fci-rule-width 2)
+(setq fci-rule-color "#83A598")
+;; ------ NeoTree
+(setq neo-theme 'nerd)
+(setq neo-window-width 25)
+;; ------ RainbowDelimiters
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+;; ------ GitGutter
 (global-git-gutter-mode t)
 ;; (git-gutter:linum-setup)
+;; ------ Spaceline
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
+(setq powerline-default-separator 'bar)
+(setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+(set-face-attribute 'spaceline-evil-emacs nil :background "#be84ff")
+(set-face-attribute 'spaceline-evil-insert nil :background "#5fd7ff")
+(set-face-attribute 'spaceline-evil-motion nil :background "#ae81ff")
+(set-face-attribute 'spaceline-evil-normal nil :background "#a6e22e")
+(set-face-attribute 'spaceline-evil-replace nil :background "#f92672")
+(set-face-attribute 'spaceline-evil-visual nil :background "#fd971f")
+(setq spaceline-buffer-size-p nil)
+(setq spaceline-buffer-encoding-abbrev-p nil)
+(spaceline-compile)
+;; ------ Diminish
+(eval-after-load "evil-commentary"
+  '(diminish 'evil-commentary-mode))
+(eval-after-load "git-gutter"
+  '(diminish 'git-gutter-mode))
+(eval-after-load "undo-tree"
+  '(diminish 'undo-tree-mode))
+(diminish 'abbrev-mode)
+;; ------ Avy
+(setq avy-style 'at-full)
+;;(setq avy-background t)
+(setq avy-lead-face "#FB4933")
 
 ;; ==========// SYNTAX //============
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; ===========// EVIL //=============
 (evil-mode t)
+(global-evil-leader-mode t)
+(evil-leader/set-leader "<SPC>")
 (setq evil-ex-search-case 'smart)
 (evil-commentary-mode t)
 (global-evil-surround-mode t)
@@ -70,6 +109,10 @@
 (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-enter)
 (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
 (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+(evil-define-key 'normal neotree-mode-map (kbd "r") 'neotree-change-root)
+(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-select-up-node)
+(evil-leader/set-key "SPC" 'neotree-toggle)
+(evil-leader/set-key "j" 'avy-goto-line)
 
 (define-key evil-normal-state-map (kbd "0") 'evil-first-non-blank)
 (define-key evil-normal-state-map (kbd "M-k") 'move-text-up)
